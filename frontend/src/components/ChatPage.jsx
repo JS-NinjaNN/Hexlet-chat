b89a9f8a9fc7964526b0bf8ae63ff71883305bd5
ToxicNN <moiseenko.artem94@yandex.ru>
2023-03-21 04:33:52 +0300

    add Formik + yup validation

---
+++ frontend/src/components/ChatPage.jsx
@@ -0,0 +1,7 @@
+// import React, { useEffect, useRef, useState } from 'react';
+
+const ChatPage = () => {
+
+};
+
+export default ChatPage;
--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -2,8 +2,9 @@
 import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 import {
-  Button, Form, Col, Card, Row, FloatingLabel,
+  Button, Form, Group, Col, Card, Row, FloatingLabel,
 } from 'react-bootstrap';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import avatarImagePath from '../assets/avatar.jpeg';
@@ -14,16 +15,32 @@ const LoginPage = () => {
   const [authFailed, setAuthFailed] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
+  const input = useRef(null);
 
   const formik = useFormik({
     initialValues: {
       username: '',
       password: '',
     },
+    validationSchema: yup.object().shape({
+      username: yup.string()
+        .min(6, 'От 6 до 20 символов')
+        .max(20, 'От 6 до 20 символов')
+        .required('Обязательное поле'),
+      password: yup.string()
+        .min(6, 'От 6 до 20 символов')
+        .max(20, 'От 6 до 20 символов')
+        .required('Обязательное поле'),
+    }),
     onSubmit: async (values) => {
       setAuthFailed(false);
+      formik.setSubmitting(false);
     },
   });
+
+  useEffect(() => {
+    input.current.focus();
+  }, []);
 
   return (
     <Row className="justify-content-center align-content-center h-100">
@@ -31,43 +48,42 @@ const LoginPage = () => {
         <Card className="shadow-sm">
           <Card.Body className="p-5 row">
             <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-              <img src={avatarImagePath} alt="Log in page" className="roundedCircle" />
+              <img src={avatarImagePath} alt="LogIn page" className="roundedCircle" />
             </div>
             <Form className="col-12 col-md-6 mt-3 mt-mb-0" onSubmit={formik.handleSubmit}>
               <h1 className="text-center mb-4">Войти</h1>
               <fieldset disabled={formik.isSubmitting}>
-                <Form.Group className="form-floating mb-3">
-                  <FloatingLabel controlId="floatingUsername" label="Ваш ник">
-                    <Form.Control
-                      type="text"
-                      onChange={formik.handleChange}
-                      value={formik.values.username}
-                      onBlur={formik.handleBlur}
-                      placeholder="username"
-                      id="username"
-                      autoComplete="username"
-                      isInvalid={authFailed}
-                      requered
-                    // ref={ }
-                    />
-                  </FloatingLabel>
+                <Form.Group className="mb-3 form-floating" controlId="username">
+                  <Form.Control
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    onBlur={formik.handleBlur}
+                    placeholder="username"
+                    autoComplete="username"
+                    isInvalid={authFailed}
+                    required
+                    ref={input}
+                  />
+                  <Form.Label>Ваш ник</Form.Label>
                 </Form.Group>
-                <Form.Group className="form-floating mb-3">
-                  <FloatingLabel controlId="floatingPassword" label="Пароль">
-                    <Form.Control
-                      type="password"
-                      onChange={formik.handleChange}
-                      value={formik.values.password}
-                      onBlur={formik.handleBlur}
-                      placeholder="password"
-                      id="password"
-                      autoComplete="current-password"
-                      isInvalid={authFailed}
-                      requered
-                    />
-                  </FloatingLabel>
-                  <Form.Control.Feedback type="invalid" className="invalid-feedback">Неверные имя пользователя или пароль</Form.Control.Feedback>
+
+                <Form.Group className="mb-4 form-floating" controlId="password">
+                  <Form.Control
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    onBlur={formik.handleBlur}
+                    placeholder="password"
+                    autoComplete="current-password"
+                    isInvalid={authFailed}
+                    required
+                    ref={input}
+                  />
+                  <Form.Label>Пароль</Form.Label>
                 </Form.Group>
+
+                <Form.Control.Feedback type="invalid" className="invalid-feedback">Неверные имя пользователя или пароль</Form.Control.Feedback>
                 <Button type="submit" variant="outline-primary" className="w-100 mb-3">Войти</Button>
               </fieldset>
             </Form>
